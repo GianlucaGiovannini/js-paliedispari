973ffd926115685a9537c9e16e019a10671ab116
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-04-01 15:10:18 +0200

    palindromo Commit

---
+++ Es_palindroma/main.js
@@ -0,0 +1,34 @@
+/* 
+    Chiedere all’utente di inserire una parola, creare una funzione per capire se la parola inserita è palindroma
+*/
+
+// chiedi all'utente di inserire una parola
+const parolaUser = prompt("inserisci una parola")
+console.log(parolaUser)
+    // dichiaro una variabile per estrapolare il risultato della function
+let palindromaSioNO;
+
+function palindromoControllo(parola_scelta) {
+    // trasformo la parola in un array
+    let parolaInArray = parolaUser.split("");
+    console.log(parolaInArray);
+
+    // invertire la sequenza dell'array 
+    let arrayElementiInvertiti = parolaInArray.reverse();
+    console.log(parolaInArray)
+
+    // unisco le lettere dell'arrai in una stringa
+    let parolaInvertita = parolaInArray.join("");
+    console.log(parolaInvertita);
+
+    // controllo se la parola dell'utente è uguale alla parola invertita
+    if (parolaUser === parolaInvertita) {
+        palindromaSioNO = "la parola è palindroma"
+    } else {
+        palindromaSioNO = "la parola non è palindroma"
+    }
+
+    return palindromaSioNO;
+}
+// stampo in console la funzione fatta sulla parola dell'utente e mi fa tornare il risultato di "palindromaSioNo"
+console.log(palindromoControllo(parolaUser))
